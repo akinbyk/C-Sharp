31673879c918cf8b6c423c1e055e742ff5d37bef
aknbyk <79026639+aknbyk@users.noreply.github.com>
2021-03-18 01:54:44 +0300

    Add files via upload

---
+++ Tek mi Çiftmi/ConsoleApplication5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi;
+            Console.WriteLine("Bir Sayı Giriniz");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            if (sayi % 2 == 0)
+            {
+                Console.WriteLine("Girdiğiniz Sayı Çifttir");
+            }
+            else
+                Console.WriteLine("Girdiğiniz Sayı Tektir");
+            Console.ReadKey();
+            }
+        }
+    }
---
+++ throw şifre/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text=="")throw new Exception("Kullanıcı Giriniz");
+                if (textBox1.Text !="æ")throw new Exception("Kullanıcı Doğru");
+                try
+                {
+                    if (textBox2.Text=="")throw new Exception("Şifre Giriniz");
+                    if (textBox2.Text!="×")throw new Exception("Şifre DOğru");
+                    MessageBox.Show("Kabul Edildi ");
+                }
+                catch(Exception ihata)
+                {
+                    MessageBox.Show(ihata.Message);
+                }
+                }
+                catch (Exception dhata)
+                {
+                    MessageBox.Show(dhata.Message);
+                }
+            }
+
+        }
+    }
+
