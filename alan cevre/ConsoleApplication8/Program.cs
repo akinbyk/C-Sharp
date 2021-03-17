e052f40a84e1f3ba0fe9a0f76cd8516b27ef57c2
aknbyk <79026639+aknbyk@users.noreply.github.com>
2021-03-18 01:26:32 +0300

    Add files via upload

---
+++ alan cevre/ConsoleApplication8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static int alanhesapla(int a, int b)
+        {
+
+            return a * b;
+        }
+        static int cevrehesapla(int a, int b)
+        {
+
+            return 2 * (a + b);
+        }
+        static void Main(string[] args)
+        {
+            int uzunkenar, kısakenar, alan, cevre;
+            Console.Write("æ Uzunkenarı Giriniz æ: ");
+            uzunkenar = Convert.ToInt32(Console.ReadLine());
+            Console.Write("× Kısakenarı Giriniz ×: ");
+            kısakenar = Convert.ToInt32(Console.ReadLine());
+            alan = alanhesapla(uzunkenar, kısakenar);
+            Console.WriteLine("µ Dikdörtgenin Alanı µ: {0}  ", alan);
+            cevre = cevrehesapla(uzunkenar, kısakenar);
+            Console.WriteLine("« Dikdörtgenin Çevresi »: {0}  ", cevre);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ alan yarıcap/ConsoleApplication2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static double alanh(int m)
+        {
+            double pi = 3.14;
+            double sonuc = pi * (m * m);
+            return sonuc;
+        }
+        static double cevreh(int m)
+        {
+            double pi = 3.14;
+            double sonuc = 2 * (pi * m);
+            return sonuc;
+        }
+        static void Main(string[] args)
+        {
+            int yarıçap;
+            Console.Write(" Yarıçapı Giriniz:");
+            yarıçap = Convert.ToInt16(Console.ReadLine());
+            double alan = alanh(yarıçap);
+            double cevre = cevreh(yarıçap);
+            Console.WriteLine(" Daire'nin Alan'ı={0}", alan);
+            Console.Write(" Daire'nin cevresi=" + cevre);
+            Console.ReadLine();
+
+
+        }
+    }
+}
